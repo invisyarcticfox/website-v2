4e81998fc1cc907ebfc056f547560c33230361f8
Lucas. <iaminvisy@gmail.com>
2025-11-28 18:15:04 +0000

    more script stuff
    clicking on blog link removes the !
    also replaced telegram link with raspi link

--- src/scripts/newBlog.ts
+++ src/scripts/newBlog.ts
@@ -1,21 +1,26 @@
 import type { blogEntries } from '@/types'
 
+
 (async () => {
   try {
     const res = await fetch('https://api.invisyarcticfox.uk/blogs')
     const [latest]:blogEntries = await res.json()
     const lsLastSeen = localStorage.getItem('lastSeenPost')
-    const blogLink = document.querySelector<HTMLElement>('.links ul li a[href="https://blog.invisyarcticfox.uk"]')
+    const blogLink = document.querySelector<HTMLAnchorElement>('.links ul li a[href="https://blog.invisyarcticfox.uk"]')!
     
     console.log('latest blog:', latest.id)
     console.log('local storage:', lsLastSeen)
     
     if ( lsLastSeen && lsLastSeen !== latest.id ) {
-      blogLink!.classList.add('new')
-      blogLink!.title = 'new blog!'
-      blogLink!.innerHTML = `<span>blog</span>`
+      blogLink.classList.add('new')
+      blogLink.title = 'new blog!'
+      blogLink.innerHTML = `<span>blog</span>`
     }
     
-    blogLink!.addEventListener('click', () => { localStorage.setItem('lastSeenPost', latest.id) })
+    blogLink.addEventListener('click', () => {
+      blogLink.classList.remove('new')
+      blogLink.title = ''
+      localStorage.setItem('lastSeenPost', latest.id)
+    })
   } catch (error) { console.error(error) }
 })()
